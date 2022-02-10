dbfe76403e23eda5f6529519c58a471b17c884ee
Earl Gresh <earl.gresh@gmail.com>
2022-02-09 18:47:09 -0800

    Include maximizer as field of module oleo
    Able to call maximizer as:
    require('oleo').maximizer.toggle()

--- lua/oleo/init.lua
+++ lua/oleo/init.lua
@@ -2,6 +2,7 @@ local module = {}
 
 local helpers = require("oleo/helpers")
 local sumneko = require("oleo/helpers/sumneko_lua")
+local maximizer = require("oleo/maximizer")
 
 -- convenience functions exported into module namespace
 for _, module_name in ipairs({ helpers, sumneko }) do
@@ -12,6 +13,7 @@ end
 
 module.helpers = helpers
 module.sumneko = sumneko
+module.maximizer = maximizer
 
 module.unload = function()
     local modules = {
@@ -23,24 +25,6 @@ module.unload = function()
     helpers.unload(modules)
 end
 
+module.maximizer.toggle()
+
 return module
-
--- print('---- PACKAGE PATH ----')
--- k
-
--- local pp = vim.fn.split(package.path, ';')
--- for k,v in ipairs(pp) do
---     print(v)
--- end
-
--- print('---- RUNTIMEPATH ----')
--- local rp = vim.fn.split(vim.o.runtimepath, ',')
--- for k,v in pairs(rp) do
---     print(v)
--- end
-
--- print('---- nvim_list_runtime_paths ----')
--- local can = vim.api.nvim_list_runtime_paths()
--- for k,v in pairs(can) do
---     print(v)
--- end
